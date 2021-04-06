b56b6fee5171d30ff57de9cb20223aada500297d
Donald R. Williams <brycewayne03.29@gmail.com>
2021-04-06 05:26:20 -0700

    CRAN changes

--- R/helpers.R
+++ R/helpers.R
@@ -1,9 +1,3 @@
-irc_met <- rbind(
-  cbind(matrix(.7, 10,10), matrix(0, 10,10)),
-  cbind(matrix(0, 10,10), matrix(0.7, 10,10))
-)
-diag(irc_met) <- 1
-
 Gamma_SS <- function(adj, k, cl){
   diag(adj ) <- 1
   
